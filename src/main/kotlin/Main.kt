86f10d637a257233da2700cc0cdaccca97a75da9
ToothLonely <arsen.shirinov2004@gmail.com>
2025-03-30 14:15:01 +0300

    KTB-04 deleted Menu class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,35 @@
 package org.example
 
+import java.io.File
+
+fun loadDictionary(fileName: String): List<Word> {
+    val words = File(fileName)
+    val dictionary = mutableListOf<Word>()
+
+    words.readLines().forEach {
+        val line = it.split('|')
+        val word = Word(line[0], line[1], line.getOrNull(2)?.toIntOrNull() ?: 0)
+
+        dictionary.add(word)
+    }
+
+    return dictionary
+}
+
 fun main() {
     val filename = "words.txt"
-    val dictionary = Menu().loadDictionary(filename)
+    val dictionary = loadDictionary(filename)
 
     while (true) {
-        Menu().show()
+        println(
+            """
+                Меню:
+                1 - Учить слова
+                2 - Статистика
+                3 - Выход
+                Выберите один из вариантов (1, 2 или 0)
+            """.trimIndent()
+        )
 
         when (readln()) {
             "1" -> println("Вы выбрали учить слова")
