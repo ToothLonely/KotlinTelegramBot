6ba800818395426d7e919ce364e4decfd691a88a
ToothLonely <arsen.shirinov2004@gmail.com>
2025-04-04 21:42:34 +0300

    KTB-07 learn words

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import java.io.File
 const val ONE_HUNDRED_PERCENT = 100
 const val MINIMUM_CORRECT_ANSWERS = 3
 const val DIFFERENCE_BETWEEN_LIST_INDEX_AND_ANSWER_INDEX = 1
-const val NUMBER_OF_WORDS_TO_ANSWER = 4
+const val NUMBER_OF_WORDS_TO_ANSWER = 3
 
 fun loadDictionary(fileName: String): List<Word> {
     val words = File(fileName)
@@ -30,8 +30,35 @@ fun showStatistic(dictionary: List<Word>) {
     println()
 }
 
+fun saveDictionary(dictionary: List<Word>, filename: String) {
+    val words = File(filename)
+    var currentData = words.readText()
+
+    words.readLines().forEach {
+        val currentLine = it.split('|')
+        val currentWord = Word(currentLine[0], currentLine[1], currentLine.getOrNull(2)?.toIntOrNull() ?: 0)
+
+        for (word in dictionary) {
+            if (word.englishWord == currentWord.englishWord && word.correctAnswerCount != currentWord.correctAnswerCount) {
+                //Тут я добавляю |0 в тех словах, где их не было
+                if (currentLine.size == 2) {
+                    currentData = currentData.replace(it, "$it|0")
+                    words.writeText(currentData)
+                }
+
+                val newLine =
+                    it.replace(currentWord.correctAnswerCount.toString(), word.correctAnswerCount.toString())
+                currentData = currentData.replace(it, newLine)
+                words.writeText(currentData)
+
+                break
+            }
+        }
+    }
+}
+
 fun learnWords(dictionary: List<Word>) {
-    while (true) {
+    outer@ while (true) {
         val notLearnedList = if (dictionary.none { it.correctAnswerCount < MINIMUM_CORRECT_ANSWERS }) {
             println("Все слова в словаре выучены")
             return
@@ -39,15 +66,48 @@ fun learnWords(dictionary: List<Word>) {
             dictionary.filter { it.correctAnswerCount < MINIMUM_CORRECT_ANSWERS }
         }
 
-        val questionWords = notLearnedList.shuffled().take(NUMBER_OF_WORDS_TO_ANSWER)
+        val questionWords = notLearnedList.shuffled().take(NUMBER_OF_WORDS_TO_ANSWER).toMutableList()
 
-        for (i in notLearnedList) {
-            println("${i.englishWord}: ")
-            questionWords.shuffled().forEachIndexed { index, word ->
+        for (currentWord in notLearnedList) {
+            if (currentWord in questionWords) continue
+
+            val correctAnswerId: Int
+            var userAnswerInput = 0
+            var userAnswerInputFlag = true
+
+            questionWords.add(currentWord)
+            val shuffledQuestionWords = questionWords.shuffled()
+            correctAnswerId =
+                shuffledQuestionWords.indexOf(currentWord) + DIFFERENCE_BETWEEN_LIST_INDEX_AND_ANSWER_INDEX
+
+            println("${currentWord.englishWord}: ")
+            shuffledQuestionWords.forEachIndexed { index, word ->
                 println("\t${index + DIFFERENCE_BETWEEN_LIST_INDEX_AND_ANSWER_INDEX} - ${word.russianWord}")
             }
+            println("\t----------")
+            println("\t0 -  Меню")
+            //println(correctAnswerId)
 
-            val answer = readln()
+            while (userAnswerInputFlag) {
+                try {
+                    userAnswerInput = readln().toInt()
+                    userAnswerInputFlag = false
+                } catch (e: NumberFormatException) {
+                    println("Ввести надо число")
+                }
+            }
+
+            if (userAnswerInput == correctAnswerId) {
+                println("Правильно!")
+                currentWord.correctAnswerCount++
+                saveDictionary(dictionary, "words.txt")
+            } else if (userAnswerInput == 0) {
+                break@outer
+            } else {
+                println("Неправильно! ${currentWord.englishWord} – это ${currentWord.russianWord}")
+            }
+
+            questionWords.remove(currentWord)
         }
 
     }
