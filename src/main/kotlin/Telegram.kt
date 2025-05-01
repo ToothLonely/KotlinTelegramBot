d22fd4047a3b89f182d7cebbec299d9723608e76
ToothLonely <arsen.shirinov2004@gmail.com>
2025-05-01 18:21:18 +0300

    KTB-17 calculate statistic only when necessary

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -28,12 +28,14 @@ fun main(args: Array<String>) {
 
         if (text?.lowercase() == "/start" || text?.lowercase() == "/menu") telegramBotService.sendMenu(chatId)
 
-        val statistic = trainer.getStatistic()
-        val statisticMessage = "Выучено ${statistic.learnedCount} из ${statistic.totalCount} слов | ${statistic.percent}%"
 
         when (menuData?.toInt()) {
             LEARN_WORDS_POINT -> telegramBotService.sendMessage(chatId, "Учить слова")
-            STATISTIC_POINT -> telegramBotService.sendMessage(chatId, statisticMessage)
+            STATISTIC_POINT -> {
+                val statistic = trainer.getStatistic()
+                val statisticMessage = "Выучено ${statistic.learnedCount} из ${statistic.totalCount} слов | ${statistic.percent}%"
+                telegramBotService.sendMessage(chatId, statisticMessage)
+            }
             EXIT_POINT -> telegramBotService.sendMessage(chatId, "Выход")
         }
 
