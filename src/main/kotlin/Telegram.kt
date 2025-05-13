9d34c55ad2157648b88705c3434a8abe4a87a2b6
ToothLonely <arsen.shirinov2004@gmail.com>
2025-05-13 22:16:21 +0300

    KTB-22 refactor chatId

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -30,45 +30,45 @@ fun handleUpdate(update: Update, trainers: HashMap<Long, LearnWordsTrainer>, tel
     val text = update.message?.text
     val chatId = update.message?.chat?.chatId ?: update.callbackQuery?.message?.chat?.chatId ?: return
     val inputData = update.callbackQuery?.data
+    telegramBotService.chatId = chatId
 
     val trainer = trainers.getOrPut(chatId) {
-        LearnWordsTrainer(filename = "$chatId.txt")
+        LearnWordsTrainer("$chatId.txt")
     }
 
     when {
         text?.lowercase() == START_COMMAND || text?.lowercase() == MENU_COMMAND || inputData == MENU_COMMAND -> {
-            telegramBotService.sendMenu(chatId)
+            telegramBotService.sendMenu()
         }
 
         inputData == LEARN_WORDS -> {
-            checkNextQuestionAndSend(trainer, telegramBotService, chatId)
+            checkNextQuestionAndSend(trainer, telegramBotService)
         }
 
         inputData == STATISTIC -> {
             val statistic = trainer.getStatistic()
             val statisticMessage =
                 "Выучено ${statistic.learnedCount} из ${statistic.totalCount} слов | ${statistic.percent}%"
-            telegramBotService.sendMessage(chatId, statisticMessage)
-            telegramBotService.sendMenu(chatId)
+            telegramBotService.sendMessage(statisticMessage)
+            telegramBotService.sendMenu()
         }
 
         inputData?.startsWith(CALLBACK_DATA_ANSWER_PREFIX) == true -> {
             val userAnswerInput = inputData.substringAfter(CALLBACK_DATA_ANSWER_PREFIX)
             when (trainer.checkAnswer(userAnswerInput.toInt())) {
-                true -> telegramBotService.sendMessage(chatId, "Правильно")
+                true -> telegramBotService.sendMessage("Правильно")
                 false -> telegramBotService.sendMessage(
-                    chatId,
                     "Неправильно! ${trainer.question.correctAnswer.englishWord} - это ${trainer.question.correctAnswer.russianWord}"
                 )
             }
             Thread.sleep(1000)
-            checkNextQuestionAndSend(trainer, telegramBotService, chatId)
+            checkNextQuestionAndSend(trainer, telegramBotService)
         }
 
         inputData == RESET_PROGRESS -> {
             trainer.resetProgress()
-            telegramBotService.sendMessage(chatId, "Прогресс сброшен")
-            telegramBotService.sendMenu(chatId)
+            telegramBotService.sendMessage("Прогресс сброшен")
+            telegramBotService.sendMenu()
         }
     }
 }
@@ -76,9 +76,8 @@ fun handleUpdate(update: Update, trainers: HashMap<Long, LearnWordsTrainer>, tel
 fun checkNextQuestionAndSend(
     trainer: LearnWordsTrainer,
     telegramBotService: TelegramBotService,
-    chatId: Long
 ) {
     val questions = trainer.getNextQuestion()
-    if (questions == null) telegramBotService.sendMessage(chatId, "Все слова уже выучены")
-    else telegramBotService.sendQuestion(chatId, questions)
+    if (questions == null) telegramBotService.sendMessage("Все слова уже выучены")
+    else telegramBotService.sendQuestion(questions)
 }
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -14,6 +14,7 @@ class TelegramBotService(
     private val botToken: String
 ) {
 
+    var chatId: Long = 0
     private val json = Json {
         ignoreUnknownKeys = true
     }
@@ -28,14 +29,14 @@ class TelegramBotService(
         return json.decodeFromString<Response>(getUpdatesResponseString).result
     }
 
-    fun sendMessage(chatId: Long, text: String?) {
+    fun sendMessage(text: String?) {
         val encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8)
         val urlSendMessage = "$urlSendMessage?chat_id=$chatId&text=$encodedText"
         val sendMessageRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(sendMessageRequest, BodyHandlers.ofString())
     }
 
-    fun sendMenu(chatId: Long) {
+    fun sendMenu() {
         val requestBody = SendMessageRequest(
             chatId = chatId,
             text = TELEGRAM_MENU,
@@ -56,7 +57,7 @@ class TelegramBotService(
         sendPOST(urlSendMessage, requestBodyString)
     }
 
-    fun sendQuestion(chatId: Long, question: Questions) {
+    fun sendQuestion(question: Questions) {
         val inlineKeyboardVariants = question.variants.mapIndexed { index, word ->
             InlineKeyboard(
                 text = word.russianWord,
